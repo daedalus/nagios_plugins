0c3265477d0fb6d44b64114d73f75c83a369b242
Dario Clavijo <daedalus2027@gmail.com>
2016-06-29 15:53:36 -0300

    cleanup

--- check_mpath.py
+++ check_mpath.py
@@ -14,18 +14,17 @@ import paramiko
 from optparse import OptionParser
 
 status = { 0:'OK' , 1:'WARNING', 2: 'CRITICAL' , 3: 'UNKNOWN'}
-
-
 last_device_count = 0
 
+# execute a remote command by ssh
 def exec_ssh(host,user,passwd,command):
-
 	ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(hostname=host, username=user, password=passwd)
         stdin, stdout, stderr = ssh.exec_command(command)
 	return stdout.readlines()
 
+# parse the results of the esxi esxcfg-mpath -l
 def parse_esxi_mpath(data):
 	global last_device_count
 	device = ""
@@ -76,6 +75,7 @@ def parse_esxi_mpath(data):
 
 	return (num_status,devices)
 
+# parse the results of the linux command multipath -l
 def parse_linux_mpath(data):
 	global last_device_count
         str_status = ''
@@ -123,6 +123,7 @@ def parse_linux_mpath(data):
 
         return ret,str_status
 
+# ssh backend
 def ssh_mpath(host,user,passwd,system):
 	if system == 'esxi':
 		command = 'esxcfg-mpath -l'
@@ -133,14 +134,15 @@ def ssh_mpath(host,user,passwd,system):
 		data = exec_ssh(host,user,passwd,command)
 		return parse_linux_mpath(data)
 	else:
-		return (3,'SYSTEM NOT IMPLEMENTED')
+		return (3,'\nSYSTEM NOT IMPLEMENTED')
 
-
+# save the results in a cache 
 def savecache(host,devices):
         fp = open('/tmp/check_mpath.%s.tmp' % host,'w')
         fp.write('devices=%s' % str(devices))
         fp.close()
 
+# read the results from the cache
 def readcache(host):
         fp = open('/tmp/check_mpath.%s.tmp' % host,'r')
         for line in fp:
@@ -150,9 +152,9 @@ def readcache(host):
                         return int(line[8:])
 
 def main():
-
 	global last_device_count
 
+	# parse earch argument into the needed options
 	parser = OptionParser()
 	parser.add_option("-H","--host",dest="host")
 	parser.add_option("-u","--user", dest="user")
@@ -175,16 +177,18 @@ def main():
 		num_status,device_summary = ssh_mpath(host,user,passwd,options.system)
 	elif options.backend == 'snmp':
 		num_status = 3
-		device_summary = 'BACKEND NOT IMPLEMENTED'
+		device_summary = '\nBACKEND STILL NOT IMPLEMENTED'
 	else:
 		num_status = 3
-                device_summary = 'BACKEND NOT IMPLEMENTED'
+                device_summary = '\nBACKEND NOT IMPLEMENTED'
 
 
 	savecache(host,last_device_count)
 
+	# output check results
 	print status[num_status] + device_summary
 	sys.exit(num_status)
 
+# Main entry point
 if __name__ == "__main__":
 	main()
