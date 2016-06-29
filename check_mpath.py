791c3f6b2de097938a2a0c389f0c4fad4e118116
Dario Clavijo <daedalus2027@gmail.com>
2016-06-29 15:46:55 -0300

    check if the number of seen paths changes over checks, caching the results

--- check_mpath.py
+++ check_mpath.py
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env python
 #
 # Nagios plugin to check the state of multipath devices for linux and esxi
 #
@@ -16,6 +16,8 @@ from optparse import OptionParser
 status = { 0:'OK' , 1:'WARNING', 2: 'CRITICAL' , 3: 'UNKNOWN'}
 
 
+last_device_count = 0
+
 def exec_ssh(host,user,passwd,command):
 
 	ssh = paramiko.SSHClient()
@@ -25,22 +27,23 @@ def exec_ssh(host,user,passwd,command):
 	return stdout.readlines()
 
 def parse_esxi_mpath(data):
+	global last_device_count
 	device = ""
 	state = ""
 	active = 0
-
-	devices = ""
-
+	devices = "\n"
 	num_status = 0
+	device_count = 0
 
 	for line in data:
 		line = line.replace('\n',"")
 		if re.search('Device: ', line):
 			device = line[11:]	
 		if re.search('State: ', line):
-			state = line[9:]
+			state = line[10:]
 		
 		if device != "" and state != "":
+			device_count += 1
 
 			if state == "dead":
 				num_status = 1
@@ -51,23 +54,34 @@ def parse_esxi_mpath(data):
 
 			if state == 'active':
 				active += 1
-		
+	
 			#print device,state	
 
-			devices = devices + "%s %s" % (device,state) 
+			devices = devices + "Device: %s Status: %s" % (device,state) 
 			devices = devices + "\n" 
 			device = ""
 			dev_state = ""
+
+	if device_count < last_device_count:
+		num_status = 1
+
+		#print 'd',device_count,last_device_count
+
+	#print active
+
 	if active < 2:
 		num_status = 2
+
+	last_device_count = device_count
 
 	return (num_status,devices)
 
 def parse_linux_mpath(data):
+	global last_device_count
         str_status = ''
-
         ret = 0
 	active = 0
+	device_count = 0
 
         def is_hex(s):
         	try:
@@ -83,12 +97,13 @@ def parse_linux_mpath(data):
 
                 if is_hex(d):
                         device = d
-                        str_status += '\ndevice %s ' % device
+                        str_status += '\nDevice: %s ' % device
+			device_count += 1
 
                 p = line.find('status=')
                 if p > 0:
                         status = line[p+7:]
-                        str_status += 'status %s ' % status
+                        str_status += 'Status: %s ' % status
 
                         if (status != 'active' and status != 'enabled'):
                                 #print 'status',status
@@ -96,10 +111,15 @@ def parse_linux_mpath(data):
 			if(status  == 'active'):
 				active += 1
 			
-		if active < 2:
-			ret = 2
+	if device_count < last_device_count:
+               	num_status = 1
+
+	if active < 2:
+		ret = 2
 
                 #str_status += '\n'
+
+	last_device_count = device_count
 
         return ret,str_status
 
@@ -115,7 +135,23 @@ def ssh_mpath(host,user,passwd,system):
 	else:
 		return (3,'SYSTEM NOT IMPLEMENTED')
 
+
+def savecache(host,devices):
+        fp = open('/tmp/check_mpath.%s.tmp' % host,'w')
+        fp.write('devices=%s' % str(devices))
+        fp.close()
+
+def readcache(host):
+        fp = open('/tmp/check_mpath.%s.tmp' % host,'r')
+        for line in fp:
+                line = line.replace('\n','')
+                p = line.find('devices=')
+                if p > -1:
+                        return int(line[8:])
+
 def main():
+
+	global last_device_count
 
 	parser = OptionParser()
 	parser.add_option("-H","--host",dest="host")
@@ -130,18 +166,24 @@ def main():
 	user = options.user
 	passwd = options.passwd
 
+	try:
+		last_device_count = readcache(host)
+	except:
+		last_device_count = -1
+
 	if options.backend == 'ssh':
-		num_status,devices = ssh_mpath(host,user,passwd,options.system)
+		num_status,device_summary = ssh_mpath(host,user,passwd,options.system)
 	elif options.backend == 'snmp':
 		num_status = 3
-		devices = 'BACKEND NOT IMPLEMENTED'
+		device_summary = 'BACKEND NOT IMPLEMENTED'
 	else:
 		num_status = 3
-                devices = 'BACKEND NOT IMPLEMENTED'
+                device_summary = 'BACKEND NOT IMPLEMENTED'
 
 
+	savecache(host,last_device_count)
 
-	print status[num_status] + "\n" + devices
+	print status[num_status] + device_summary
 	sys.exit(num_status)
 
 if __name__ == "__main__":
